73be14f7b47d19d4f17f3894a4a17bbc0eea8394
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-26 12:57:24 +0100

    created the service and controller class within their respective packages

---
+++ src/main/java/com/qa/baespring/controller/pokedexController.java
@@ -0,0 +1,5 @@
+package com.qa.baespring.controller;
+
+public class pokedexController {
+
+}
---
+++ src/main/java/com/qa/baespring/service/pokedexService.java
@@ -0,0 +1,8 @@
+package com.qa.baespring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class pokedexService {
+
+}
