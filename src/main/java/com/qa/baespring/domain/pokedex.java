12f3d0076900b32f1dc4a3c3fb6e0d08c8eebe9d
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-26 11:22:39 +0100

    generated the too string and hash code and equals methods

--- src/main/java/com/qa/baespring/domain/pokedex.java
+++ src/main/java/com/qa/baespring/domain/pokedex.java
@@ -1,5 +1,7 @@
 package com.qa.baespring.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -101,8 +103,33 @@ public class pokedex {
 		this.weakness = weakness;
 	}
 
+	@Override
+	public String toString() {
+		return "pokedex [id=" + id + ", name=" + name + ", moves=" + moves + ", type=" + type + ", weakness=" + weakness
+				+ "]";
+	}
 	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, moves, name, type, weakness);
+	}
+
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		pokedex other = (pokedex) obj;
+		return id == other.id && Objects.equals(moves, other.moves) && Objects.equals(name, other.name)
+				&& Objects.equals(type, other.type) && Objects.equals(weakness, other.weakness);
+	}
+
 	
+
 	
 }
