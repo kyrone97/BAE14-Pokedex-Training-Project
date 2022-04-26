77afe5103cd5a653cc82c85a0dbbc8304daf2032
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-26 11:07:13 +0100

    created pokdex class in domain package

---
+++ src/main/java/com/qa/baespring/domain/pokedex.java
@@ -0,0 +1,11 @@
+package com.qa.baespring.domain;
+
+import javax.persistence.Entity;
+
+
+//used for modelling data into tables(maps from java/spring into database tables)
+//will create the table in the database if it does not already exist
+@Entity 
+public class pokedex {
+
+}
