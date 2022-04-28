f7f8357763c37a4eecbf9fa2c929ac350d292485
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-28 15:57:23 +0100

    created function to load dummy data

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,1 +1,56 @@
-alert("connected")
+const pokemon = [
+    {   
+        _id: 1,
+        name: "mike",
+        type: "Dark",
+        moves: "Bite",
+        weakness: "Rock",
+
+    },
+    {
+        _id: 2,
+        name: "Blake",
+        type: "Fire",
+        moves: "Blaze", 
+        weakness: "Water",
+
+    },
+    {
+        _id: 3,
+        name: "Dwight",
+        type: "Fairy",
+        moves: "Steel Tail",
+        weakness: "Fire",
+
+    },
+
+];
+
+function savePokemon(){
+    alert("calling savePokemon");
+}
+
+
+function loadpokemon(){
+    let tableBodyContent = " ";
+
+    pokemon.forEach(pokemon => {
+        tableBodyContent += `
+                <tr>
+                    <td>${pokemon._id}</td>
+                    <td>${pokemon.name}</td>
+                    <td>${pokemon.type}</td>
+                    <td>${pokemon.moves}</td>
+                    <td>${pokemon.weakness}</td>
+                    <td class = "w-25">
+                        <button class="btn btn-warning">Update</button> |
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+    `;
+    });
+    const tableBody = document.getElementById('table-body');
+    tableBody.innerHTML += tableBodyContent;   
+}
+
+loadpokemon();
